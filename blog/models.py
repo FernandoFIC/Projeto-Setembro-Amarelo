9df190966e1eaed3dc2fd5946f1762010e3f695b
Davi Rocha <163659635+davirfb@users.noreply.github.com>
2025-08-02 20:14:43 -0300

    modelos criados,vinculando com o BD

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+class Project(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    content = models.TextField()
+    active = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='projects/')
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name 
